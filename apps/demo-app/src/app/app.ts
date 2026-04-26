1a893733b6a28e4d3fbf631e38fe3bcbd1ba50e9
Mike Fellowes <mike@appframeworks.com>
2026-04-26 10:07:45 +0000

    Connected demo angular app to demo dotnet api using proxy

--- apps/demo-app/src/app/app.ts
+++ apps/demo-app/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcome } from './nx-welcome';
+import { WeatherForecast } from './weather-forecast';
 
 @Component({
-  imports: [NxWelcome, RouterModule],
+  imports: [NxWelcome, RouterModule, WeatherForecast],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
